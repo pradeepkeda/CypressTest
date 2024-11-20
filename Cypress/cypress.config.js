3f297d6ee812763cc996a38cbd042311e33ff655
Pradeep <pradeep.tieto@gmail.com>
2024-11-20 16:50:09 +0530

    'Cypress'

---
+++ Cypress/cypress.config.js
@@ -0,0 +1,39 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: "ocnw76",
+  e2e: {
+
+    baseUrl: "https://naveenautomationlabs.com/opencart/index.php?route=account/login",
+    setupNodeEvents(on, config) {
+
+      /*  on('before:browser:launch', (browser = {}, launchOptions) => {
+          if (browser.family === 'chromium' && browser.name !== 'electron') {
+            launchOptions.args.push("--incognito");
+            return launchOptions
+          }
+        })
+  
+        on('before:browser:launch', (browser, launchOptions) => {
+          if (browser.family === 'chromium') {
+            launchOptions.args.push('--disable-logging');
+            return launchOptions;
+          }
+        });
+  
+  */
+    },
+    // "proxyServer": false,
+    chromeWebSecurity: false,
+
+
+  },
+
+
+
+  chromeWebSecurity: false,
+
+  watchForFileChanges: false,
+
+
+});
